af447a64c05c7988e6353216e2029f2210f6b702
Jenny Strømmen <jennys1994@gmail.com>
2022-02-07 19:51:56 +0100

    Add demo

--- src/main/java/tasks/Demo.java
+++ src/main/java/tasks/Demo.java
@@ -10,15 +10,30 @@ public class Demo {
     // TODO: Check if you have solved the task by running this method!
     public static void main(String[] args) {
         Demo demo = new Demo();
-        demo.doIt(false);
+        demo.doIt();
         Checker.checkDemo();
     }
 
-    private void doIt(boolean bool) {
-        // TODO: DEMO
-        // TODO: How can the statement be printed when doIt is given false as input on line 12?
-        if (bool) ;{
-            System.out.println("Uh oh, this should not print!");
+    // TODO: DEMO
+    // TODO: How can the the "content" in class Bag be null?
+    // TODO: Make changes to Bag such that content no longer is null.
+    @NoEqualsMethod
+    class Bag {
+
+        private String content;
+
+        public String getContent() {
+            return this.content;
+        }
+    }
+
+    private void doIt() {
+        Bag bag = new Bag();
+        if (bag.getContent() != null){
+            System.out.println(bag);
+        } else {
+            System.out.println("Uh oh, the variable is " + bag.getContent());
+            Checker.tell("Uh oh, the variable is " + bag.getContent());
         }
     }
 
